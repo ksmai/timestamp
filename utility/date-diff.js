4ea77e4586164a06de56baef42fee5d32662c180
ksmai <kasingmai@gmail.com>
2017-01-21 22:45:17 +0800

    Create date-diff utility

---
+++ utility/date-diff.js
@@ -0,0 +1,22 @@
+'USE STRICT';
+var moment = require('moment');
+
+// takes 2 Date objects, return their difference in various units
+module.exports = function(date1, date2) {
+  if( !date1 || !date2 || !date1.getTime || !date1.getTime() ||
+      !date2.getTime || !date2.getTime()
+  ) {
+    return null;
+  }
+  var d1 = moment(date1), d2 = moment(date2);
+  if(d1.diff(d2) > 0) [d1, d2] = [d2, d1];
+  return {
+    year:        d2.diff(d1, 'years', true),
+    month:       d2.diff(d1, 'months', true),
+    day:         d2.diff(d1, 'days', true),
+    hour:        d2.diff(d1, 'hours', true),
+    minute:      d2.diff(d1, 'minutes', true),
+    second:      d2.diff(d1, 'seconds', true),
+    millisecond: d2.diff(d1)
+  };
+};
---
+++ utility/date-diff.test.js
@@ -0,0 +1,35 @@
+'USE STRICT';
+var assert = require('assert');
+var dateDiff = require('./date-diff');
+
+describe('dateDiff', function() {
+  it('calculates difference between 2 date/time objects', function() {
+    var d1 = new Date(2017,1,21,13,42,42);
+    var d2 = new Date(2018,3,6,9);
+    var diff = dateDiff(d1, d2);
+    var
+      y = 1,
+      m = y * 12 + 1,
+      d = y * 365 + 43,
+      h = d * 24 + 19,
+      i = h * 60 + 17,
+      s = i * 60 + 18,
+      ms = 1000 * s;
+    assert.ok( !!diff );
+    assert.equal( Math.floor(diff.year), y );
+    assert.equal( Math.floor(diff.month), m );
+    assert.equal( Math.floor(diff.day), d );
+    assert.equal( Math.floor(diff.hour), h );
+    assert.equal( Math.floor(diff.minute), i );
+    assert.equal( Math.floor(diff.second), s );
+    assert.equal( Math.floor(diff.millisecond), ms );
+  });
+    
+  it('accounts for leap year', function() {
+    var d1 = new Date(2016,2,1);
+    var d2 = new Date(2016,1,1);
+    var diff = dateDiff(d1, d2);
+    assert.ok( !!diff );
+    assert.equal( ~~diff.day, 29 );
+  });
+});
