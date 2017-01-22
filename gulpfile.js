c7dc29d679c87a45f91c5a1f5eb03b75c6332f07
ksmai <kasingmai@gmail.com>
2017-01-22 17:15:45 +0800

    Update gulpfile for client-side scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp');
+var browserify = require('gulp-browserify');
 var minifyHTML = require('gulp-minify-html');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function() {
   gulp
     .src(['./server/**/*.test.js', './utility/**/*.test.js'])
     .pipe(mocha());
+});
+
+gulp.task('mincss', function() {
+  gulp
+    .src('./client/css/*.css')
+    .pipe(cleanCSS())
+    .pipe(concat('style.css'))
+    .pipe( gulp.dest('./public') );
 });
 
 gulp.task('minhtml', function() {
@@ -17,9 +28,18 @@ gulp.task('minhtml', function() {
     .pipe( gulp.dest('./public') );
 });
 
-gulp.task('build', ['minhtml']);
+gulp.task('browserify', function() {
+  gulp
+    .src('./client/app.js')
+    .pipe(browserify())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('build', ['browserify', 'minhtml', 'mincss']);
 
 gulp.task('watch', ['build'], function() {
   gulp.watch('./client/**/*.html', ['minhtml']);
+  gulp.watch('./client/css/*.css', ['mincss']);
+  gulp.watch('./client/**/*.js', ['browserify']);
   gulp.watch(['./server/**/*.js', './utility/**/*.js'], ['test']);
 });
